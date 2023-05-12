a55c009658a65990a71c3cdb91fe369cb7e45568
Patricia Atanasoff <system.informatica365@gmail.com>
2023-05-12 13:09:07 -0300

    loader when submitting form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 /*--lightbox--*/
 
 var lightbox = new SimpleLightbox('.container-gallery a', { /* options */ });
+
+
+/*--loader formulario--*/
+
+const form = document.querySelector(".contact-form")
+
+document.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const loader = document.querySelector(".contact-form-loader"),
+        response = document.querySelector(".contact-form-response");
+
+    loader.classList.remove("none");
+
+    setTimeout(() => {
+        loader.classList.add("none");
+        response.classList.remove("none");
+        form.reset()
+
+        setTimeout(() => response.classList.add("none"), 3000)
+
+    }, 3000)
+})

c5b28fa9e511b63121a94a0def94212e968c7e25
Patricia Atanasoff <system.informatica365@gmail.com>
2023-05-12 02:50:54 -0300

    swiper js

---
+++ js/swiper.js
@@ -0,0 +1,39 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    effect: 'coverflow',
+    slidesPerView: 1,
+    coverflowEffect: {
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+    },
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+    },
+     // Media queries >= 600px
+    breakpoints: {
+        600: {
+            slidesPerView: 3,
+        },
+    }
+});
